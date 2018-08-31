ddbd79466f972fa1cea4df60e7dc5bc64f1f9e7a
Nick Moore <nicholas.moore5@icloud.com>
2018-08-31 13:02:22 -0400

    Fetch command operational

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,19 @@ function fetchData(url) {
 }
 
 Promise.all([
-  fetchData('https://randomuser.me/api/')
-
-]).then(data => console.log(data))
+  fetchData('https://randomuser.me/api/?format=json')
+  ]).then(data => {
+    const bioPic = data[0].results[0].picture.thumbnail;
+    const name = data[0].results[0].name.first.charAt(0).toUpperCase() + data[0].results[0].name.first.slice(1) +
+      ' ' +
+      data[0].results[0].name.last.charAt(0).toUpperCase() + data[0].results[0].name.last.slice(1);
+    const email = data[0].results[0].email;
+    const location = data[0].results[0].location.city.charAt(0).toUpperCase() + data[0].results[0].location.city.slice(1);
+    const cellNumber = data[0].results[0].phone;
+    const address = data[0].results[0].location.street + ', ' + location + ' ' + data[0].results[0].location.postcode;
+    const birthday = data[0].results[0].dob.date;
+    console.log(birthday);
+})
 
 /*
   .then(data => {
@@ -40,6 +50,26 @@ function checkStatus(response) {
   } else {
     return Promise.reject(new Error(response.statusText));
   }
+}
+/*
+function generateGalley (data) {
+  const galleyBio = `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">data[0].results[0].gender</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+        </div>
+    </div>
+  `;
+}
+
+window.onload = function (){
+  generateGalley();
+
 }
 
 /*
